106e4c188f232678e4d9f315da94ac5b0812f93a
David Bunca <bunca.david@gmail.com>
2024-12-08 19:41:42 +0100

    -text added

--- VPWA_Chatterly_Bunca_Daniel_BE/app/services/ws.ts
+++ VPWA_Chatterly_Bunca_Daniel_BE/app/services/ws.ts
@@ -1,3 +1,4 @@
+//Pri implementácii socket.io sme sa inšpirovali článkom https://medium.com/@armyabakouan/how-to-configure-socket-io-for-adonisjs-v6-4afbcaa1a465
 import { Server } from 'socket.io'
 import server from '@adonisjs/core/services/server'
 
